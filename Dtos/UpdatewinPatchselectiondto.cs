fa0e9fa886d39a55dc97d5c4bb531809ce457a2a
BRIJMOHAN SINGH <brijmohansingh02081993@gmail.com>
2026-04-07 11:46:10 +0530

    change  updatepatchselection model

--- Dtos/UpdatewinPatchselectiondto.cs
+++ Dtos/UpdatewinPatchselectiondto.cs
@@ -7,6 +7,7 @@
         public int locationid { get; set; }
         public string selectedIds { get; set; }
         public string domainids { get; set; }
+        public string deviceIds { get; set; }
 
     }
 }
